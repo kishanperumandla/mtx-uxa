1e0d412e5581d34c1533110fae55d494f4a50f9f
Kishan <kishanpermand@gmail.com>
2025-02-06 23:07:39 +0530

    Updated Render website URL in clinet Config file

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,2 +1,6 @@
-export const BASE_URL = 'http://127.0.0.1:3000/api/v1'
-export const BASE_ORIGIN = 'http://127.0.0.1:3000'
+// export const BASE_URL = 'http://127.0.0.1:3000/api/v1'
+// export const BASE_ORIGIN = 'http://127.0.0.1:3000'
+export const BASE_URL = 'https://mtx-uxa.onrender.com/api/v1'
+export const BASE_ORIGIN = 'https://mtx-uxa.onrender.com'
+
+// https://mtx-uxa.onrender.com
